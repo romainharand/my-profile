f8eaf6e00a94c0a3ad67a2db2f3fe791629ca5ef
romainharand <44243451+romainharand@users.noreply.github.com>
2021-02-22 17:01:41 +0100

    Update

--- test-data-analytics.js
+++ test-data-analytics.js
@@ -11,31 +11,6 @@
       var ATconfig = {};
       ATconfig.site = 617553;
       var ATTag = new window.ATInternet.Tracker.Tag(ATconfig);
-      ATTag.page.set({
-        name: #PAGE_NAME#,
-        level2: 1,
-    	chapter1: "chapter1",
-    	chapter2: "chapter2",
-    	chapter3: "chapter3",
-        customObject: {
-    	param1: {
-      	param11: 'testCustomObject1',
-      	param12: 'testCustomObject2'
-    }  
-  }});
-	ATTag.customVars.set({
-    site: {
-        1: "prod",
-        2: "rouge",
-        3: "grande",
-      	4: 3,
-      	5: 1.3,
-    page: {
-        1: "pagevariable1",
-        2: "pagevariable2",
-        3: "pagevariable3"}
-    }});
-       if (typeof mvTesting && set !== "undefined"){
         ATTag.mvTesting.set({
         test: 'First AB test MV testing',
         waveId: 1,
@@ -43,14 +18,7 @@
     });
     	ATTag.mvTesting.add({
         variable: 'test-variable',
-        version: 'test-version'
-        })}
-ATTag.selfPromotion.set({
-   impression:{
-      adId: '1',
-      format: 'test-auto-promo-format'
-   }
-});   
+        version: #TESTMVTESTING#})  
       ATTag.dispatch();
     }
     };
