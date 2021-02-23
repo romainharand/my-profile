232f18b0b9ddcd965ee9190a450aae33e25037a8
romainharand <44243451+romainharand@users.noreply.github.com>
2021-02-23 13:51:17 +0100

    Update

---
+++ public/cmp/js/tarteaucitronInit.js
@@ -0,0 +1,30 @@
+tarteaucitron.init({
+    "privacyUrl": "", /* URL de la politique de confidentialité */
+
+    "hashtag": "#tarteaucitron", /* Ouvrez le panneau avec ce hashtag */
+    "cookieName": "tarteaucitron", /* Nom du cookie */
+
+    "orientation": "middle", /* Postion de la bannière (top - bottom) */
+    "showAlertSmall": true, /* Afficher la petite bannière en bas à droite */
+    "cookieslist": true, /* Afficher la liste des cookies */
+
+    "adblocker": false, /* Afficher une alerte si un adblocker est détécté */
+    "DenyAllCta" : true, /* Afficher un boutton pour refuser tous les cookies */
+    "AcceptAllCta" : true, /* Afficher un boutton pour accepter tous les cookies (quand highPrivacy est true) */
+    "highPrivacy": true, /* Désactiver l'auto consentement */
+    "handleBrowserDNTRequest": false, /* Si l'utilisateur à activer Do Not Track, interdire tout les cookies */
+
+    "removeCredit": false, /* Supprimer le lien de crédit - laissez le pour nous soutenir ;) */
+    "moreInfoLink": true, /* Afficher le lien "plus d'info) */
+    "useExternalCss": false, /* Si c'est faut, le fichier tarteaucitron.css par défaut sera charger */
+
+    //"cookieDomain": ".my-multisite-domaine.fr", /* Cookie partagé pour le site Web du sous-domaine */
+
+    "readmoreLink": "/cookiespolicy", /* Changer le lien par défaut "en savoir plus" pointant vers tarteaucitron.io */
+    
+    "mandatory": false /* Afficher un message sur les cookies obligatoires */
+});
+
+// Google Fonts
+tarteaucitron.user.googleFonts = 'families';
+(tarteaucitron.job = tarteaucitron.job || []).push('googlefonts');
