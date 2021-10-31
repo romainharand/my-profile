29906f441384c39bfc65ee0c17d424109e27cc62
romainharand <44243451+romainharand@users.noreply.github.com>
2021-10-31 13:22:01 +0000

    Update

--- test-data-analytics.js
+++ test-data-analytics.js
@@ -1,33 +1,3 @@
-
-  (function(){
-            var at = document.createElement('script');
-            at.type = 'text/javascript';
-            at.async = true;
-            at.src = '//tag.aticdn.net/617553/smarttag.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] || document.getElementsByTagName('script')[0].parentNode).insertBefore(at, null);
-        })(); 
-  window.ATInternet = {
-    onTrackerLoad:function(){
-      var ATconfig = {};
-      ATconfig.site = 617553;
-      var ATTag = new window.ATInternet.Tracker.Tag(ATconfig);
-        ATTag.mvTesting.set({
-        test: 'First AB test MV testing',
-        waveId: 1,
-        creation: 'test-creation'
-    });
-    	ATTag.mvTesting.add({
-        variable: 'test-variable',
-        version: #TESTMVTESTING#})  
-      ATTag.dispatch();
-    }
-    };
-
-
-
-
-
-	
 var tarteaucitron = document.cookie;
 if (tarteaucitron.includes('kameleoon=true')) {
   console.log("optin");
